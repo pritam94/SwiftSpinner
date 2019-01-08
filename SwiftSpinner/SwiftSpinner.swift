1e92cf889badf424832ed01d013bb06829d61bf0
Vineet Kumar Singh <vineetkr.999@gmail.com>
2019-01-08 21:27:23 +0530

    Swift 4 language bug fixes

--- SwiftSpinner/SwiftSpinner.swift
+++ SwiftSpinner/SwiftSpinner.swift
@@ -58,7 +58,7 @@ public class SwiftSpinner: UIView {
         outerCircle.lineWidth = 8.0
         outerCircle.strokeStart = 0.0
         outerCircle.strokeEnd = 0.45
-        outerCircle.lineCap = .round
+        outerCircle.lineCap = = kCALineCapRound
         outerCircle.fillColor = UIColor.clear.cgColor
         outerCircle.strokeColor = outerCircleDefaultColor
         outerCircleView.layer.addSublayer(outerCircle)
@@ -75,7 +75,7 @@ public class SwiftSpinner: UIView {
         innerCircle.lineWidth = 4.0
         innerCircle.strokeStart = 0.5
         innerCircle.strokeEnd = 0.9
-        innerCircle.lineCap = .round
+        innerCircle.lineCap = = kCALineCapRound
         innerCircle.fillColor = UIColor.clear.cgColor
         innerCircle.strokeColor = innerCircleDefaultColor
         innerCircleView.layer.addSublayer(innerCircle)
@@ -177,7 +177,7 @@ public class SwiftSpinner: UIView {
                 NotificationCenter.default.addObserver(
                     spinner,
                     selector: #selector(SwiftSpinner.updateFrame),
-                    name: UIApplication.didChangeStatusBarOrientationNotification,
+                    name: NSNotification.Name.UIApplicationDidChangeStatusBarOrientation,
                     object: nil)
             #endif
         } else if spinner.dismissing {
@@ -488,7 +488,7 @@ public class SwiftSpinner: UIView {
     @objc public func updateFrame() {
         if let containerView = SwiftSpinner.containerView() {
             SwiftSpinner.sharedInstance.frame = containerView.bounds
-            containerView.bringSubviewToFront(SwiftSpinner.sharedInstance)
+            containerView.bringSubview(toFront: SwiftSpinner.sharedInstance)
         }
     }
    
